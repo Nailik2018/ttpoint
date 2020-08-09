7460764e07c87854baa286167a857c32ef54b1b6
SURFACE-BOOK-KI\kilua <kilualthaus@hotmail.com>
2020-08-09 15:07:29 +0200

    navbar implementation, implementation startseite ranking und verbände

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AssociationsComponent } from './landingpage/associations/associations.component';
+import { RankingsComponent } from './landingpage/rankings/rankings.component';
+import { NavigationNavbarComponent } from './navigation-navbar/navigation-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    AssociationsComponent,
+    RankingsComponent,
+    NavigationNavbarComponent
   ],
   imports: [
     BrowserModule,
